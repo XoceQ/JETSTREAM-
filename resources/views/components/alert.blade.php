1112ee7f337fe442ca6fae721b453c8226537d11
XoceQ <dcurbanxtreme@gmail.com>
2022-05-25 17:36:41 -0500

    update

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,4 @@
+<div class="#f5f5dc bg-orange-100 border-1-4 border-orange-500 text-orange-700 p-4" role="alert">
+  <p class="font-bold">Be warned</p>
+  <p>Something not deal might be happening.</p>
+</div>
